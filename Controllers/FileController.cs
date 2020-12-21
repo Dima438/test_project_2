7e04d30e148b0cc796a8397e341dce267f34acf2
AnusManus <bfskinner60659@gmail.com>
2020-12-21 18:15:41 +0300

    last commit before branching

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -15,18 +15,22 @@ namespace DBStuff.Controllers
 {
     public class FileController : Controller
     {
-
+        //use controller's request to manually fetch the file
+        //[FromRoute] / [FromQuery]
         [HttpPost]
         [Route("api/file")]
-        public ActionResult<string> Upload(IFormFile file, [FromServices] IHostingEnvironment environment) //what's this? 
+        public ActionResult<string> Upload([FromForm] IFormFile file, [FromServices] IHostingEnvironment environment) //what's this? 
         {
             // string test_path = $"{environment.ContentRootPath}";
 
             // Console.WriteLine(test_path + "?");
 
+            var item = Request.Form.Files[0];
+
             try
             {
-                string fileName = $"{environment.ContentRootPath}\\FileTest\\{file.FileName}";
+                // string fileName = $"{environment.ContentRootPath}\\FileTest\\{file.FileName}";
+                string fileName = $"{environment.ContentRootPath}\\FileTest\\{item.FileName}";
 
                 Console.WriteLine(fileName);
 
