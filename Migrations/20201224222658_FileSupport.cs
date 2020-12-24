a3f79e462a45b70ba5f3c6c6e0e014df89b34710
AnusManus <bfskinner60659@gmail.com>
2020-12-25 01:30:33 +0300

    working on it

---
+++ Migrations/20201224222658_FileSupport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace db_stuff.Migrations
+{
+    public partial class FileSupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Content",
+                table: "Records",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Records");
+        }
+    }
+}
